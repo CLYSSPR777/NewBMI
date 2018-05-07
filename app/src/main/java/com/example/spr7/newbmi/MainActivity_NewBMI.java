646f099ffe591b7b58fdf1be0a27b22d902bcdc9
CLYSSPR777 <clys129@gmail.com>
2018-05-08 02:24:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/spr7/newbmi/MainActivity_NewBMI.java
@@ -0,0 +1,13 @@
+package com.example.spr7.newbmi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity_NewBMI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__new_bmi);
+    }
+}
